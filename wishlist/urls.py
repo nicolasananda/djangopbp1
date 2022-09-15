5a71720d4d9c3c2be9a49ec96218b2e109200a5f
Nicolas Ananda <nico.halim03@gmail.com>
2022-09-15 11:21:33 +0700

    commit

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_xml #customize to the name of the function created
+from wishlist.views import show_json # adjust the name of the function created
+from wishlist.views import show_json_by_id #customise the name of the function created
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'), #customize the name of the function created
+    path('json/', show_json, name='show_json'), #customise the name of the function created
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #customise the name of the function created
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
 
@@ -8,5 +10,21 @@ def show_wishlist(request):
     'name': 'NicolasAnanda'
     }
     return render(request, "wishlist.html", context)
+    
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ItemsWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request):
+    data = ItemsWishlist.objects.filter(pk=id)
+    // JSON Format
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+    // XML Format
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 # Create your views here.
