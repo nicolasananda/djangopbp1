b71aec9671ce96d7d75abdb7a3448dfa347d7994
Nicolas Ananda <nico.halim03@gmail.com>
2022-10-07 11:57:06 +0700

    Tutorial Update

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,9 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_xml #customize to the name of the function created
 from wishlist.views import show_json # adjust the name of the function created
 from wishlist.views import show_json_by_id #customise the name of the function created
+from wishlist.views import register # Customize with the name of the function created
+from wishlist.views import login_user  # Customize with the name of the function created
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -11,4 +14,7 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), #customize the name of the function created
     path('json/', show_json, name='show_json'), #customise the name of the function created
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #customise the name of the function created
+    path('register/', register, name='register'), # Customize with the name of the function created
+    path('login/', login_user, name='login'),  # Customize with the name of the function created
+    path('logout/', logout_user, name='logout'),  # Customize with the name of the function created
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -2,14 +2,62 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('wishlist:login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # login first
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # create response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # create last_login cookie and add it to response
+            return response
+        else:
+            messages.info(request, 'Wrong Username or Password!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
     'list_item': data_wishlist_item,
-    'name': 'NicolasAnanda'
+    'name': 'NicolasAnanda',
+    'last_login': request.COOKIES['last_login']
     }
     return render(request, "wishlist.html", context)
+
+
     
 def show_xml(request):
     data = ItemWishlist.objects.all()
@@ -21,10 +69,8 @@ def show_json(request):
 
 def show_json_by_id(request):
     data = ItemsWishlist.objects.filter(pk=id)
-    // JSON Format
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-    // XML Format
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 # Create your views here.
